3f59dd24bec55a7a78544a6f2a9d9c394cb86de6
Braian Rosas <braianrosas.dev@outlook.com>
2020-11-24 13:48:41 -0300

    update

--- scripts/type_it_header.js
+++ scripts/type_it_header.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     new TypeIt("#type-it", {
-        strings: "Entérate de los beneficios de contratar personal <span>administrativo</span> en otro país",
+        strings: "Entérate los beneficios de contratar personal <span>administrativo</span> en otro país",
         speed: 50,
         cursor: false
     })
